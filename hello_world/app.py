19f82502426dd6ae7ce04f058a1e5b8bbb7d20f1
Taylor M <melnikprod@gmail.com>
2023-03-02 21:27:48 -0500

    Baisc whatever

--- hello_world/app.py
+++ hello_world/app.py
@@ -35,6 +35,9 @@ def lambda_handler(event, context):
 
     return {
         "statusCode": 200,
+        "headers": {
+        "Access-Control-Allow-Origin": "*"
+    },
         "body": json.dumps({
             "message": "hello world",
             # "location": ip.text.replace("\n", "")
